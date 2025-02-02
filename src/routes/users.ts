b816bd9c175174df5712faae312e6bf315d3abe2
Matheus <matheusteodorogarcia@gmail.com>
2025-02-02 15:03:16 -0300

    feat(users): add POST route to create a new user

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,7 +1,27 @@
 import { FastifyInstance } from "fastify";
+import knex from "knex";
+import { randomUUID } from "node:crypto";
+import { z } from "zod";
 
 export async function usersRoutes(app: FastifyInstance) {
-  app.get("/", (response, reply) => {
-    return reply.send("Retorno dos usuários");
+  app.post("/", async (request, reply) => {
+    const createUserBodySchema = z.object({
+      name: z.string(),
+    });
+
+    try {
+      const { name } = createUserBodySchema.parse(request.body);
+
+      await knex("transactions").insert({
+        id: randomUUID(),
+        name,
+      });
+
+      return reply.status(201).send();
+    } catch (error) {
+      console.error(error);
+
+      return reply.status(400).send();
+    }
   });
 }

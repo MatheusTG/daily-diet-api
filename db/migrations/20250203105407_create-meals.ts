014b5b503c8ced2171a11af410c610f8ccb8e07d
Matheus <matheusteodorogarcia@gmail.com>
2025-02-03 08:07:29 -0300

    feat(db): create meals table

---
+++ db/migrations/20250203105407_create-meals.ts
@@ -0,0 +1,17 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable("meals", (table) => {
+    table.uuid("id").primary();
+    table.string("name");
+    table.string("description");
+    table.timestamp("datetime").defaultTo(knex.fn.now()).notNullable();
+    table.boolean("diet");
+    table.uuid("session_id").index();
+    table.timestamp("created_at").defaultTo(knex.fn.now()).notNullable();
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable("meals");
+}

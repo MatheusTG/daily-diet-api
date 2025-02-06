376cfc82dfcc04d317ffdc50aa5eeb0e2a47a7fa
Matheus <matheusteodorogarcia@gmail.com>
2025-02-06 08:13:57 -0300

    refactor(routes): move application routes to app.ts

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,5 @@
-import fastify from "fastify";
-import { usersRoutes } from "./routes/users";
 import { env } from "./env";
-import cookie from "@fastify/cookie";
-import { mealsRoutes } from "./routes/meals";
-
-const app = fastify();
-
-app.register(cookie);
-
-app.register(usersRoutes, {
-  prefix: "users",
-});
-
-app.register(mealsRoutes, {
-  prefix: "meals",
-});
+import { app } from "./app";
 
 app
   .listen({

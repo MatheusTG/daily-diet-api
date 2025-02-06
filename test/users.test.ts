dff522ae2fb5042d5be5e5c9a469afc360f91761
Matheus <matheusteodorogarcia@gmail.com>
2025-02-06 15:41:30 -0300

    test(users): add test for user fetch route

--- test/users.test.ts
+++ test/users.test.ts
@@ -2,6 +2,7 @@ import { afterAll, beforeAll, beforeEach, describe, it } from "vitest";
 import request from "supertest";
 import { app } from "../src/app";
 import { execSync } from "node:child_process";
+import { knex } from "../src/database";
 
 describe("Users routes", () => {
   beforeAll(async () => {
@@ -16,6 +17,7 @@ describe("Users routes", () => {
     execSync("npm run knex migrate:rollback --all");
     execSync("npm run knex migrate:latest");
   });
+
   it("should be able to create a new user", async () => {
     await request(app.server)
       .post("/users")
@@ -23,5 +25,20 @@ describe("Users routes", () => {
         name: "Amanda",
       })
       .expect(201);
+  });
+
+  it("should be able to fetch a user", async () => {
+    const createUserResponse = await request(app.server)
+      .post("/users")
+      .send({
+        name: "Amanda",
+      })
+      .expect(201);
+
+    const cookies = createUserResponse.get("Set-Cookie");
+
+    if (!cookies) throw new Error("Cookies do not exists!");
+
+    await request(app.server).get("/users").set("Cookie", cookies).expect(200);
   });
 });

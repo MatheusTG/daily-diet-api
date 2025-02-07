38639a42c213f76bcba1dfe077c21f0e0a67c450
Matheus <matheusteodorogarcia@gmail.com>
2025-02-06 21:41:12 -0300

    test(meals): add test to meal list route

--- test/meals.test.ts
+++ test/meals.test.ts
@@ -1,5 +1,5 @@
 import { beforeEach, describe } from "node:test";
-import { afterAll, beforeAll, it } from "vitest";
+import { afterAll, beforeAll, expect, it } from "vitest";
 import request from "supertest";
 import { app } from "../src/app";
 import { execSync } from "node:child_process";
@@ -18,7 +18,7 @@ describe("Meals routes", () => {
     execSync("npm run knex -- migrate:latest");
   });
 
-  it.only("should be able to create a new meal", async () => {
+  it("should be able to create a new meal", async () => {
     const userCreateResponse = await request(app.server)
       .post("/users")
       .send({
@@ -40,5 +40,43 @@ describe("Meals routes", () => {
         diet: false,
       })
       .expect(201);
+  });
+
+  it("should be able to list meals", async () => {
+    const userCreateResponse = await request(app.server)
+      .post("/users")
+      .send({
+        name: "Amanda",
+      })
+      .expect(201);
+
+    const cookies = userCreateResponse.get("Set-Cookie");
+
+    if (!cookies) throw new Error("Cookies do not exists!");
+
+    await request(app.server)
+      .post("/meals")
+      .set("Cookie", cookies)
+      .send({
+        name: "Janta",
+        description: "Arroz, Feijão e Frango",
+        datetime: "2024-02-05T15:30:00Z",
+        diet: false,
+      })
+      .expect(201);
+
+    const mealsListResponse = await request(app.server)
+      .get(`/meals/list/${cookies[0].replace("sessionId=", "").split(";")[0]}`)
+      .set("Cookie", cookies)
+      .expect(200);
+
+    expect(mealsListResponse.body.data).toEqual([
+      expect.objectContaining({
+        name: "Janta",
+        description: "Arroz, Feijão e Frango",
+        datetime: "2024-02-05T15:30:00Z",
+        diet: 0,
+      }),
+    ]);
   });
 });
